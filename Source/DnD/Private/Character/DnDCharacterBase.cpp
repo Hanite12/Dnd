b6f945dbd4b67477d1da4eab2e6c21d92fb42e66
unknown <kirillafanasev@gmail.com>
2025-06-23 21:33:30 +0300

    First BPCharacters

--- Source/DnD/Private/Character/DnDCharacterBase.cpp
+++ Source/DnD/Private/Character/DnDCharacterBase.cpp
@@ -7,6 +7,9 @@ ADnDCharacterBase::ADnDCharacterBase()
 {
  	 	PrimaryActorTick.bCanEverTick = false;
 
+		Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+		Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+		Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ADnDCharacterBase::BeginPlay()
@@ -14,3 +17,4 @@ void ADnDCharacterBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
+ 
