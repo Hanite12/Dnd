7ac86f8e9c1363a9d7d8ac27570576085171afd4
unknown <kirillafanasev@gmail.com>
2025-06-25 02:34:26 +0300

    Player Controller

--- Source/DnD/Private/Player/DnDPlayerController.cpp
+++ Source/DnD/Private/Player/DnDPlayerController.cpp
@@ -3,3 +3,53 @@
 
 #include "Player/DnDPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+#include  "EnhancedInputComponent.h"
+
+
+ADnDPlayerController::ADnDPlayerController()
+{
+	bReplicates = true;
+}
+
+void ADnDPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(DnDContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(DnDContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void ADnDPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ADnDPlayerController::Move);
+}
+
+void ADnDPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
--- Source/DnD/Public/Player/DnDPlayerController.h
+++ Source/DnD/Public/Player/DnDPlayerController.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerController.h"
 #include "DnDPlayerController.generated.h"
 
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -13,5 +18,18 @@ UCLASS()
 class DND_API ADnDPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ADnDPlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override; 
 	
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> DnDContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
